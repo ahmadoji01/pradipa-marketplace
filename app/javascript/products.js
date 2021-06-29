945398a0ed4e0f1abd0b7761418f0b3bbe1b96a5
uzzybotak <uzzybotak@users.noreply.github.com>
2021-06-29 07:06:29 +0700

    feature: Change Active Product Gallery Slide According to Active Variants

--- app/javascript/products.js
+++ app/javascript/products.js
@@ -3,3 +3,8 @@ window.sortChange = function(val) {
     url.searchParams.set('sort', val);
     window.location = url;
 }
+
+window.changeActiveSlide = function(index) {
+    var slider = $( '.product-gallery-slider' );
+    slider[0].slick.slickGoTo(index);
+}
