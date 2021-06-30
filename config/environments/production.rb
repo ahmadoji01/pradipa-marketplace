a2606e8da80d3a99bf58ae3d7e877a52d577b54b
uzzybotak <uzzybotak@users.noreply.github.com>
2021-06-30 12:29:11 +0700

    build: Set Assets Compile to True

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,9 @@ Rails.application.configure do
   config.assets.js_compressor = :terser
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  #config.assets.digest = true
-  #config.public_file_server.enabled = true
+  config.assets.compile = true
+  config.assets.digest = true
+  config.public_file_server.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
