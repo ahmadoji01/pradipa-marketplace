02b833b5f6a653c550c10c030109bdccccb072df
uzzybotak <uzzybotak@users.noreply.github.com>
2021-07-03 18:53:47 +0700

    refactor: Unused Slider Reduction on JS and CSS

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -2,6 +2,4 @@ class Spree::Blog < ApplicationRecord
     mount_uploader :featured_image, BlogFeaturedImageUploader
     belongs_to :blog_category, optional: true
     has_rich_text :body
-    
-    Gutentag::ActiveRecord.call self
 end
