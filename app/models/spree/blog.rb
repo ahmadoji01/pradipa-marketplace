b97690e96546ee3b6340b250474e8826690ead62
uzzybotak <uzzybotak@users.noreply.github.com>
2021-07-03 17:37:26 +0700

    feature: Add Gutentag Rails Tag Library
    Also refactor: Remove Unused Javascripts and CSS

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -2,4 +2,6 @@ class Spree::Blog < ApplicationRecord
     mount_uploader :featured_image, BlogFeaturedImageUploader
     belongs_to :blog_category, optional: true
     has_rich_text :body
+    
+    Gutentag::ActiveRecord.call self
 end
