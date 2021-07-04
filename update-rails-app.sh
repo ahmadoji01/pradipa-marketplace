a4423a61d62ac7a1f251ca60b4988baf73be2cc7
uzzybotak <uzzybotak@users.noreply.github.com>
2021-07-04 07:15:51 +0700

    build: Add Assets Clean and Restart NGINX on Update Rails Scripts

--- update-rails-app.sh
+++ update-rails-app.sh
@@ -1,1 +1,3 @@
-docker-compose run app bash -c 'SECRET_KEY_BASE=1 RAILS_ENV=production bundle exec rails assets:precompile'
+docker-compose run app bash -c 'RAILS_ENV=production bundle exec rails assets:clean'
+docker-compose run app bash -c 'RAILS_ENV=production bundle exec rails assets:precompile'
+docker-compose restart nginx
