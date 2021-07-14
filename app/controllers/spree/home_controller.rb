2c5f7c43fa96e7548b72bc2cbf221ac60ea00d54
uzzybotak <uzzybotak@users.noreply.github.com>
2021-07-14 14:45:50 +0700

    Update home_controller.rb

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -1,16 +1,16 @@
 module Spree
-    class HomeController < Spree::StoreController
-      helper 'spree/products'
-      respond_to :html
-  
-      def index
-        @searcher = build_searcher(params.merge(include_images: true))
-        @products = @searcher.retrieve_products
-        @taxonomies = Spree::Taxonomy.includes(root: :children)
-        @categories = Spree::Taxon.find_by(name: "Categories").children
-        @featured_products = @products.order("created_at DESC").slice(0,5)
-        @recent_products = @products.order("created_at DESC").slice(0,8)
-        @recent_posts = Spree::Blog.last(5)
-      end
+  class HomeController < Spree::StoreController
+    helper 'spree/products'
+    respond_to :html
+
+    def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @products = @searcher.retrieve_products
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+      @categories = Spree::Taxon.find_by(name: "Categories").children
+      @featured_products = @products.order("created_at DESC").slice(0,5)
+      @recent_products = @products.order("created_at DESC").slice(0,8)
+      @recent_posts = Spree::Blog.last(5)
     end
   end
+end
