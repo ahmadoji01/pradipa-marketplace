2fd6759c7cdf4b15fdf0cb6604d9e1e22503ac4e
uzzybotak <uzzybotak@users.noreply.github.com>
2021-08-23 16:25:22 +0700

    fix: Internal Server Error on Contact Us Page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     get '/track-my-package', :to => 'static_pages#track_my_package', :as => 'track_my_package_page'
     get '/contact-us', :to => 'static_pages#contact_us', :as => 'contact_us_page'
 
+    post '/submit_ticket', :to => 'static_pages#submit_ticket'
+
     get '/producer_dashboard', :to => 'producer_dashboard#redirect_to_home'
     get '/producer_dashboard/home', :to => 'producer_dashboard#index', :as => 'producer_dashboard_home_page'
     get '/producer_dashboard/brand_info', :to => 'producer_dashboard#brand_info', :as => 'producer_dashboard_brand_info_page'
