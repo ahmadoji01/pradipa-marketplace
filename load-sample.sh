5cfb8509a84771ff99467c76c4215e9aa0c8af1f
uzzybotak <uzzybotak@users.noreply.github.com>
2021-09-02 21:18:35 +0700

    Build: Reconfigure Shell Script for App Initialization on Deployment

--- load-sample.sh
+++ load-sample.sh
@@ -1,2 +1,4 @@
-docker-compose run app bash -c 'bin/spring stop'
-docker-compose run app bash -c 'RAILS_ENV=production rails spree_sample:load'
+docker-compose run --rm web bash -c '
+    bin/spring stop &&
+    RAILS_ENV=production rails spree_sample:load
+'
