640b0e922a3fc56517862b82cc6237dd81f91763
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2021-12-28 12:58:56 +0700

    feature: Add Coming Soon Checker
    Rule is, a user is allowed to access the admin panel for logging in. If he is an admin, then he can access everything. Otherwise redirect to coming soon page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,24 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   def coming_soon
-    if ENV["RAILS_COMING_SOON"] == "true"
-      redirect_to root_path
+    if ENV["RAILS_COMING_SOON"] == "true" && !request.path.include?("admin")
+      if spree_current_user.nil?
+        redirect_to main_app.coming_soon_page_path
+        return
+      end
+      
+      if !spree_current_user.role_users.first.role.name == "admin"       
+        redirect_to main_app.coming_soon_page_path
+        return
+      end
+      return
     end
+
   end
+
+  private
+    def current_controller?(names)
+      names.include?(controller_name)
+    end
+
 end
