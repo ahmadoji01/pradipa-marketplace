d096798792726633e52da3c7c6b6eea9775e65fa
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2022-09-30 21:53:05 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,39 +4,30 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   def coming_soon
-    if ENV["RAILS_COMING_SOON"] != "true"
+    if ENV["RAILS_COMING_SOON"] == "true"
+      if request.path.include?("submit_mailing")
+        return
+      end
+  
+      if request.path.include?("admin")
+        if spree_current_user.nil?
+          redirect_to main_app.coming_soon_page_path
+          return
+        end
+
+        if spree_current_user.role_users.first.role.name != "admin"
+          redirect_to main_app.coming_soon_page_path
+          return
+        end
+        return
+      end
+  
+      if request.path.include?("blogs")
+        return
+      end
       return
     end
-
-    if request.path.include?("submit_mailing")
-      return
-    end
-
-    if request.path.include?("admin")
-      return
-    end
-
-    if request.path.include?("collection")
-      return
-    end
-
-    if request.path.include?("blogs")
-      return
-    end
-
-    if spree_current_user.nil?
-      redirect_to main_app.coming_soon_page_path
-      return
-    end
-
-    if spree_current_user.role_users.first.role.name != "admin"
-      redirect_to main_app.coming_soon_page_path
-      return
-    end
-
-    redirect_to main_app.coming_soon_page_path
     return
-
   end
 
   private
